ac7949c45d55966fad4abdc8363d5f1ade10acee
Éder Barreto <eder.barreto.oliveira@hotmail.com>
2021-03-13 19:37:05 -0300

    Initial commit

---
+++ SwiftQuiz/SwiftQuiz/Quiz.swift
@@ -0,0 +1,31 @@
+//
+//  Quiz.swift
+//  SwiftQuiz
+//
+//  Created by Eder Barreto on 13/03/21.
+//  Copyright © 2021 Eder Barreto. All rights reserved.
+//
+
+import Foundation
+
+class Quiz {
+    
+    let question: String
+    let options: [String]
+    private let correctedAnswer: String
+    
+    init(question: String, options: [String], correctedAnswer: String) {
+        self.question = question
+        self.options = options
+        self.correctedAnswer = correctedAnswer
+    }
+    
+    func validateOption(_ index: Int) -> Bool {
+        let answer = options[index]
+        return answer == correctedAnswer
+    }
+    
+    deinit {
+        print("Liberou quiz da memória")
+    }
+}
---
+++ SwiftQuiz/SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Eder Barreto on 13/03/21.
+//  Copyright © 2021 Eder Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
